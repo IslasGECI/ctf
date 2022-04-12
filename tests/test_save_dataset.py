054e5bfec1274fd9816ddf4f74050c77d2473138
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-04-12 14:40:20 -0700

    ♻ Extract function `_remove_if_exists()`

--- tests/test_save_dataset.py
+++ tests/test_save_dataset.py
@@ -1,6 +1,7 @@
 from ..ctf import Referee
-import os
 import pandas as pd
+
+from .utils_tests import _remove_if_exists
 
 path_to_submission_directory = "tests/test_dataset_pollos_petrel/"
 path_to_complete_dataset = path_to_submission_directory + "complete_dataset.csv"
@@ -10,8 +11,7 @@ referee = Referee(path_to_complete_dataset)
 
 def test_save_training_dataset_includes_na():
     path_to_training = referee.get_training_path()
-    if os.path.exists(path_to_training):
-        os.remove(path_to_training)
+    _remove_if_exists(path_to_training)
     referee.save_training_dataset()
     obtained_count_na = pd.read_csv(path_to_training).isna().sum().sum()
     expected_count_na = 31
